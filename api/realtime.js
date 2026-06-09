9032c451e6f3a5a64f8bd52d23c6075ba56a49f3
PetiteDan <petitedan@gmail.com>
2026-06-09 10:16:12 +0800

    Add files via upload

--- api/realtime.js
+++ api/realtime.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
   try {
     const response = await fetch('https://fhy.wra.gov.tw/WraApi/v1/Water/RealTimeInfo');
     const data = await response.json();
-    const IDS = ['1140H136', '1140H131', '1140H118', '1140H055', '1140H502', '1140H508'];
+    const IDS = ['1140H507', '1140H504', '1140H502', '1140H110', '1140H503', '1140H508'];
     const filtered = data.filter(s => IDS.includes(s.StationNo));
     res.status(200).json(filtered);
   } catch (e) {

3e3bda3413354dab06c96ccb99fc148d2218158c
PetiteDan <petitedan@gmail.com>
2026-06-09 10:19:12 +0800

    Add files via upload

--- api/station.js
+++ api/station.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
   try {
     const response = await fetch('https://fhy.wra.gov.tw/WraApi/v1/Water/Station');
     const data = await response.json();
-    const IDS = ['1140H507', '1140H504', '1140H502', '1140H110', '1140H503', '1140H508'];
+    const IDS = ['1140H507', '1140H504', '1140H502'];
     const filtered = data.filter(s => IDS.includes(s.StationNo));
     res.status(200).json(filtered);
   } catch (e) {
